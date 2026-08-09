5d4ad6ffeced163842eeacefc2f5195d959371d5
zain100000 <muhammadzainulabideen292@gmail.com>
2026-08-09 11:59:41 +0500

    Update animated number value in About section from 6 to 8

--- src/pages/about.js
+++ src/pages/about.js
@@ -147,7 +147,7 @@ xl:text-center md:text-lg sm:text-base xs:text-sm"
               {/* Stat Block 3 */}
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumberFramerMotion value={6} />+
+                  <AnimatedNumberFramerMotion value={8} />+
                 </span>
                 <h2
                   className="mb-0 xl:mb-4 text-xl font-medium capitalize text-dark/75 dark:text-light/75 
